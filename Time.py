74dc5025546c66bb88be3e5e965885f72cf66b58
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2020-05-15 14:03:28 +0300

    Add files via upload

--- Time.py
+++ Time.py
@@ -5,9 +5,15 @@ import time
 import Commands
 command=Commands.Commands()
 class Time: 
+    def timer_start():
+        threading.Timer(30.0, timer_start).start()
+        try:
+            asyncio.run_coroutine_threadsafe(save_data(),bot.loop)
+        except Exception as exc:
+            pass
     #schedule.every().hour.do(command.get_currencies)
-    schedule.every().day.at("16:30").do(command.get_currencies)
+    #schedule.every().day.at("16:30").do(command.get_currencies)
 
-    while True:
-       schedule.run_pending()
-       time.sleep(1)
+   # while True:
+       #schedule.run_pending()
+       #time.sleep(1)
